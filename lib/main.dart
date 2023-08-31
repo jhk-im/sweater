b82e8df6ef0c5832506740337cc8f5dfcc280c74
jhk-im <kjhun200@gmail.com>
2023-08-31 23:23:19 +0900

    feat: add home widget package

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,67 @@ import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'package:sweater/viewmodel/weather_main_view_model.dart';
 
+import 'package:flutter/foundation.dart';
+import 'package:home_widget/home_widget.dart';
+import 'package:workmanager/workmanager.dart';
+
+import 'dart:async';
+import 'dart:math';
+
+
+
+/// Called when Doing Background Work initiated from Widget
+@pragma("vm:entry-point")
+void backgroundCallback(Uri? data) async {
+  print(data);
+
+  if (data?.host == 'titleclicked') {
+    final greetings = [
+      'Hello',
+      'Hallo',
+      'Bonjour',
+      'Hola',
+      'Ciao',
+      '哈洛',
+      '안녕하세요',
+      'xin chào'
+    ];
+    final selectedGreeting = greetings[Random().nextInt(greetings.length)];
+
+    await HomeWidget.saveWidgetData<String>('title', selectedGreeting);
+    await HomeWidget.updateWidget(
+        name: 'HomeWidgetExampleProvider', iOSName: 'HomeWidgetExample');
+  }
+}
+
+/// Used for Background Updates using Workmanager Plugin
+@pragma("vm:entry-point")
+void callbackDispatcher() {
+  Workmanager().executeTask((taskName, inputData) {
+    final now = DateTime.now();
+    return Future.wait<bool?>([
+      HomeWidget.saveWidgetData(
+        'title',
+        'Updated from Background',
+      ),
+      HomeWidget.saveWidgetData(
+        'message',
+        '${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}',
+      ),
+      HomeWidget.updateWidget(
+        name: 'HomeWidgetExampleProvider',
+        iOSName: 'HomeWidgetExample',
+      ),
+    ]).then((value) {
+      return !value.contains(false);
+    });
+  });
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: kDebugMode);
 
   // dotenv
   await dotenv.load(fileName: 'assets/.env');
--- lib/view/screen/weather_main_screen.dart
+++ lib/view/screen/weather_main_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:home_widget/home_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:sweater/main.dart';
+import 'package:sweater/repository/source/remote/model/fcst.dart';
 import 'package:sweater/view/widget/loading_widget.dart';
 import 'package:sweater/view/widget/main/main_grid_view_widget.dart';
 import 'package:sweater/view/widget/main/main_today_list_view_widget.dart';
 import 'package:sweater/view/widget/main/main_top_widget.dart';
 import 'package:sweater/view/widget/main/main_weeks_list_view_widget.dart';
 import 'package:sweater/viewmodel/weather_main_view_model.dart';
+import 'package:workmanager/workmanager.dart';
+
+
 
 class WeatherMainScreen extends StatefulWidget {
   const WeatherMainScreen({Key? key}) : super(key: key);
@@ -17,7 +24,9 @@ class WeatherMainScreen extends StatefulWidget {
 class _WeatherMainScreenState extends State<WeatherMainScreen> {
   final ScrollController _controller = ScrollController();
 
-  bool isScrollDown = false;
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
 
   @override
   void initState() {
@@ -27,18 +36,104 @@ class _WeatherMainScreenState extends State<WeatherMainScreen> {
         isScrollDown = _controller.offset > 0.0;
       });
     });
+    HomeWidget.setAppGroupId('YOUR_GROUP_ID');
+    HomeWidget.registerBackgroundCallback(backgroundCallback);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _checkForWidgetLaunch();
+    HomeWidget.widgetClicked.listen(_launchedFromWidget);
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _titleController.dispose();
+    _messageController.dispose();
     super.dispose();
   }
+
+  Future _updateWidget() async {
+    try {
+      return HomeWidget.updateWidget(
+          name: 'HomeWidgetExampleProvider', iOSName: 'HomeWidgetExample');
+    } on PlatformException catch (exception) {
+      debugPrint('Error Updating Widget. $exception');
+    }
+  }
+
+  Future _loadData() async {
+    try {
+      return Future.wait([
+        HomeWidget.getWidgetData<String>('title', defaultValue: 'Default Title')
+            .then((value) => _titleController.text = value ?? ''),
+        HomeWidget.getWidgetData<String>('message',
+            defaultValue: 'Default Message')
+            .then((value) => _messageController.text = value ?? ''),
+      ]);
+    } on PlatformException catch (exception) {
+      debugPrint('Error Getting Data. $exception');
+    }
+  }
+
+  void _checkForWidgetLaunch() {
+    HomeWidget.initiallyLaunchedFromHomeWidget().then(_launchedFromWidget);
+  }
+
+  void _launchedFromWidget(Uri? uri) {
+    if (uri != null) {
+      showDialog(
+          context: context,
+          builder: (buildContext) => AlertDialog(
+            title: const Text('App started from HomeScreenWidget'),
+            content: Text('Here is the URI: $uri'),
+          ));
+    }
+  }
+
+  void _startBackgroundUpdate() {
+    Workmanager().registerPeriodicTask('1', 'widgetBackgroundUpdate',
+        frequency: Duration(minutes: 15));
+  }
+
+  void _stopBackgroundUpdate() {
+    Workmanager().cancelByUniqueName('1');
+  }
+
+  bool isScrollDown = false;
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<WeatherMainViewModel>();
     final state = viewModel.state;
+
+    Future _sendData() async {
+      try {
+        return Future.wait([
+          HomeWidget.saveWidgetData<String>('title', viewModel.state.address?.region1depthName ?? '',),
+          HomeWidget.saveWidgetData<String>('message', '${viewModel.state.todayTmpList[0].fcstValue ?? ''}°'),
+          HomeWidget.saveWidgetData<String>('status', viewModel.state.skyList.isNotEmpty ? viewModel.state.skyList[0].fcstValue ?? '' : '',),
+          HomeWidget.saveWidgetData<String>('min_max', '${_getTemperatureFromFcst(viewModel.state.tmnList)} / ${_getTemperatureFromFcst(viewModel.state.tmxList)}'),
+          HomeWidget.renderFlutterWidget(
+            const Icon(
+              Icons.flutter_dash,
+              size: 200,
+            ),
+            logicalSize: Size(200, 200),
+            key: 'dashIcon',
+          ),
+        ]);
+      } on PlatformException catch (exception) {
+        debugPrint('Error Sending Data. $exception');
+      }
+    }
+
+    Future<void> _sendAndUpdate() async {
+      await _sendData();
+      await _updateWidget();
+    }
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
@@ -49,9 +144,12 @@ class _WeatherMainScreenState extends State<WeatherMainScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               direction: Axis.vertical,
               children: [
-                MainTopWidget(
-                  state: state,
-                  isScrollDown: isScrollDown,
+                InkWell(
+                  onTap: _sendAndUpdate,
+                  child: MainTopWidget(
+                    state: state,
+                    isScrollDown: isScrollDown,
+                  ),
                 ),
                 Expanded(
                   flex: 1,
@@ -71,5 +169,13 @@ class _WeatherMainScreenState extends State<WeatherMainScreen> {
         ),
       ),
     );
+  }
+
+  String _getTemperatureFromFcst(List<Fcst> fcstList) {
+    if (fcstList.isNotEmpty) {
+      Fcst fcst = fcstList[0];
+      return '${fcst.fcstValue?.substring(0, fcst.fcstValue!.length - 2) ?? ''}°';
+    }
+    return '';
   }
 }
